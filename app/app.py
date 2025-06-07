18f232bea8ebf59e76f20f7d0c9a638c7abbb8d6
sudrellle <sudrelleesther@gmail.com>
2025-06-07 13:16:11 +0100

    ajout

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-
+import time
 def page2():
     st.title("Second page")
     st.header('st.slider')
@@ -12,6 +12,22 @@ def page2():
      'Select a range of values',
      0.0, 100.0, (25.0, 75.0))
     st.write('Values:', values)
+    st.subheader('Range time slider')
+
+    appointment = st.slider(
+        "Schedule your appointment:",
+        value=(time(11, 30), time(12, 45)))
+    st.write("You're scheduled for:", appointment)
+
+    # Example 4
+
+    st.subheader('Datetime slider')
+
+    start_time = st.slider(
+        "When do you start?",
+        value=datetime(2020, 1, 1, 9, 30),
+        format="MM/DD/YY - hh:mm")
+    st.write("Start time:", start_time)
 
 
 pg = st.navigation([
